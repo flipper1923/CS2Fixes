571d74cd6af30df95802bc6ca50e75940ed1ee82
mihaigsm2003 <51678688+mihaigsm2003@users.noreply.github.com>
2023-10-26 20:16:05 +0300

    Update detours.cpp

--- src/detours.cpp
+++ src/detours.cpp
@@ -213,7 +213,7 @@ void FASTCALL Detour_Host_Say(CCSPlayerController *pController, CCommand &args, 
 		}
 	}
 
-	if (*args[1] == '!' || *args[1] == '/')
+	if (*args[1] == '!' || *args[1] == '/'|| *args[1] == '@')
 		ParseChatCommand(args[1], pController);
 }
 
