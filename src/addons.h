0762d234fc1c8506a848a6ef7849e324ada32e6b
AZzeL <69967985+AZzeLx@users.noreply.github.com>
2023-10-29 18:04:18 +0200

    Update addons.h

--- src/addons.h
+++ src/addons.h
@@ -42,23 +42,9 @@ CON_COMMAND_CHAT(rs, "reset your score")
 	player->m_iScore = 0;
 	player->m_iMVPs = 0;
 
-	ClientPrint(player, HUD_PRINTTALK, " \7[Reset Score]\1 You successfully reset your score.");
+	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXS " \3%s \1You successfully reset your score.", player->GetPlayerName());
 }
-CON_COMMAND_CHAT(RS, "reset your score")
-{
-	if (!player)
-		return;
-	
-	player->m_pActionTrackingServices->m_matchStats().m_iKills = 0;
-	player->m_pActionTrackingServices->m_matchStats().m_iDeaths = 0;
-	player->m_pActionTrackingServices->m_matchStats().m_iAssists = 0;
-	player->m_pActionTrackingServices->m_matchStats().m_iDamage = 0;
-	player->m_iScore = 0;
-	player->m_iMVPs = 0;
 
-	ClientPrint(player, HUD_PRINTTALK, " \7[Reset Score]\1 You successfully reset your score.");
-}
-//************************************end reset**************************************************************
 CON_COMMAND_CHAT(ws, "fake ws")
 {
     if (!player)
@@ -66,76 +52,7 @@ CON_COMMAND_CHAT(ws, "fake ws")
 
     ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXS "\3%s\1, Play 2h to have access to !ws.", player->GetPlayerName());
 }
-CON_COMMAND_CHAT(help, "help")
-{
-		if (!player)
-		return;
-ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXS "Use commands: !medic, !rs,!RS, !sound, !stats, !vip, !stats, /u(admins chat)");
-}
-CON_COMMAND_CHAT(vip, "vip info")
-{
-	if (!player)
-		return;
 
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXV"\1Starting health: \5 100-115.");
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXV"\1Starting armor: \5 110-200.");
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXV"\1Money add every round: \5 1000-5000.");
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXV"\1Starting with: \5 defeuser, he, smoke, molotov, flashbang, healthshot .");
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXV"\1Smoke color: \5 green, \14blue, \7red, \2r\4a\3n\5d\6o\7m.");
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXV"\1For buying VIP, contact \7FOUNDER.");
-}
-
-//****************************************MEDIC********************************************************
-CON_COMMAND_CHAT(medic, "medic")
-{
-	if (!player)
-		return;
-
-	int health = 0;
-	int iPlayer = player->GetPlayerSlot();
-
-	Z_CBaseEntity* pEnt = (Z_CBaseEntity*)player->GetPawn();
-
-	//ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"pZEPlayer testing...");
-
-	ZEPlayer* pZEPlayer = g_playerManager->GetPlayer(iPlayer);
-	if (!pZEPlayer)
-	{
-		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"pZEPlayer not valid.");
-		return;
-	}
-
-	//ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"pZEPlayer valid.");
-
-	if (pEnt->m_iHealth() < 1)
-	{
-		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"You need to be alive in order to use medkit.");
-		return;
-	}
-	
-	if (pZEPlayer->WasUsingMedkit())
-	{
-		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"You already used your medkit in this round");
-		return;
-	}
-
-		if (pEnt->m_iHealth() > 99)
-	{
-		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"You have enough life.");
-		return;
-	}
-
-	health = pEnt->m_iHealth() + 50;
-
-	if (health > 100)
-		health = 100;
-
-	pEnt->m_iHealth = health;
-
-	pZEPlayer->SetUsedMedkit(true);
-
-	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIXM"Medkit used! Your health is now %d", health);
-}
 CON_COMMAND_CHAT(stats, "get your stats")
 {
 	if (!player)
